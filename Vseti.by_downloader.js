d94865a92a08119225e54a4173904a82368aa171
Alexander Pitkin <peleccom@gmail.com>
2014-07-22 23:25:54 +0300

    fist commit

---
+++ Vseti.by_downloader.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name       Vseti.by downloader
+// @namespace  http://peleccom.appspot.com/vseti
+// @version    0.1
+// @description  This script help to download video from vseti.by
+// @match      http://vseti.by/*
+// @copyright  2012+, Peleccom
+// ==/UserScript==
+
+(function (window, undefined) {
+    var TIMEOUT = 5000;
+    var marker_class = "direct_link_extracted";
+
+    var divider_el = $('<span class="divider">|</span>');
+    var download_el = $('<a class="mv_hide_info" style="color: rgb(255, 255, 255);">Скачать</a>');
+    
+    var img = '<div style="background-color: black;color:white; width: 22px; height: 22px;"></div>';
+
+    function run() {
+        window.setInterval(video_search, TIMEOUT);
+    }
+
+    function video_search() {
+        var video = $('.video_box').not('.'+marker_class);
+        video.each(
+            function (){
+                $(this).addClass(marker_class);
+                var config = $("param[name='flashvars']", this).val();
+                var json = config.substr(7);
+                var obj = JSON.parse(json);
+                var url = obj.clip.url;
+                url = unescape(url);
+                var controls_line = $("#mv_controls_line");
+                add_download_button(controls_line, url);
+            }
+            );
+    }
+    
+    
+    function add_download_button(element, url){
+        var local_download_el = download_el.clone();
+        local_download_el.attr("href", url);
+        element.append(divider_el.clone());
+        element.append(local_download_el);
+    }
+
+run();
+})(window);
+
